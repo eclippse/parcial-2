f6d0ef238809c5698347e4d764cc2a1570c8b562
franks <frank.zabala@uao.edu.co>
2020-04-28 15:15:07 -0500

    Updates

---
+++ precache-manifest.cd9fab79d06cc715763c21a3fed7304b.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e8d1f5c7dbd77434499fe635dcce9f",
+    "url": "/parcial-2/index.html"
+  },
+  {
+    "revision": "5b8330c2baf1d876613b",
+    "url": "/parcial-2/static/css/main.e0597e85.chunk.css"
+  },
+  {
+    "revision": "55c779ef6f198bb171e9",
+    "url": "/parcial-2/static/js/2.e48afffe.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/parcial-2/static/js/2.e48afffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b8330c2baf1d876613b",
+    "url": "/parcial-2/static/js/main.e417a077.chunk.js"
+  },
+  {
+    "revision": "941e2811d0cf9da95ea8",
+    "url": "/parcial-2/static/js/runtime-main.3e0822a7.js"
+  },
+  {
+    "revision": "028a52b207bc775ba16258efd608d4b7",
+    "url": "/parcial-2/static/media/alita.028a52b2.jpg"
+  },
+  {
+    "revision": "9d7717730f0b79fba15c96215a04aad8",
+    "url": "/parcial-2/static/media/android.9d771773.jpg"
+  },
+  {
+    "revision": "ccb0bc774f32486c78169a5337e2bf37",
+    "url": "/parcial-2/static/media/arduino.ccb0bc77.jpg"
+  },
+  {
+    "revision": "577dfc33edbc08accdb7a4aebaf5008f",
+    "url": "/parcial-2/static/media/aztecas.577dfc33.jpg"
+  },
+  {
+    "revision": "f863152bff1980018a72dbc23728594c",
+    "url": "/parcial-2/static/media/blender.f863152b.jpg"
+  },
+  {
+    "revision": "ef77c0383ff1c1392d733454da3a831e",
+    "url": "/parcial-2/static/media/cause.ef77c038.jpg"
+  },
+  {
+    "revision": "908090d2db6ef3ae66a275ba34e87e3f",
+    "url": "/parcial-2/static/media/chijiro.908090d2.jpg"
+  },
+  {
+    "revision": "f4ec3a7163f5a0bf642a1cf56d3e2271",
+    "url": "/parcial-2/static/media/crash.f4ec3a71.jpg"
+  },
+  {
+    "revision": "a003df080eb1146b39937d9082396b07",
+    "url": "/parcial-2/static/media/dhammapada.a003df08.jpg"
+  },
+  {
+    "revision": "e786aea058853d8e47ee6fde291af3ab",
+    "url": "/parcial-2/static/media/dioses.e786aea0.jpg"
+  },
+  {
+    "revision": "9eb136dca04e9ce4058cb82a23126caa",
+    "url": "/parcial-2/static/media/dracula.9eb136dc.jpg"
+  },
+  {
+    "revision": "e959457536b96e8cbf9f2ca27722e871",
+    "url": "/parcial-2/static/media/dragon.e9594575.jpg"
+  },
+  {
+    "revision": "c5e300610f8817e58df0cb34c9b5d087",
+    "url": "/parcial-2/static/media/emocional.c5e30061.jpg"
+  },
+  {
+    "revision": "df471bcb9a50376fee2b1f748e1bd1ff",
+    "url": "/parcial-2/static/media/funerales.df471bcb.jpg"
+  },
+  {
+    "revision": "ead7de78a88cf0314bac60ed83a02184",
+    "url": "/parcial-2/static/media/gimp.ead7de78.jpg"
+  },
+  {
+    "revision": "7aa9a6279d2ee52da13af197a8cf611c",
+    "url": "/parcial-2/static/media/git.7aa9a627.jpg"
+  },
+  {
+    "revision": "1fa4d8ed8f94713dc63240fe06e54111",
+    "url": "/parcial-2/static/media/gta.1fa4d8ed.jpg"
+  },
+  {
+    "revision": "5c5cf413c5499b8a56e4bfac2b291d57",
+    "url": "/parcial-2/static/media/guapa.5c5cf413.jpg"
+  },
+  {
+    "revision": "cac135b07ec349fdf0adce51df8a500e",
+    "url": "/parcial-2/static/media/halo.cac135b0.jpg"
+  },
+  {
+    "revision": "069f6dcef57efa692b802c66bb74a0fa",
+    "url": "/parcial-2/static/media/ilusiones.069f6dce.jpg"
+  },
+  {
+    "revision": "74edb6ed30e253c3db93371542f6e6f5",
+    "url": "/parcial-2/static/media/isla.74edb6ed.jpg"
+  },
+  {
+    "revision": "a0d285727e3ece54e25f4106dc0249ab",
+    "url": "/parcial-2/static/media/joker.a0d28572.jpg"
+  },
+  {
+    "revision": "70e4768fc658e179c2cdc0fdefb97c47",
+    "url": "/parcial-2/static/media/logo.70e4768f.png"
+  },
+  {
+    "revision": "6c5939f7c9e5cae9b15476c011aaa333",
+    "url": "/parcial-2/static/media/mario.6c5939f7.jpg"
+  },
+  {
+    "revision": "98bae19fd90851e9e4cc29e4a12c0887",
+    "url": "/parcial-2/static/media/mariposa.98bae19f.jpg"
+  },
+  {
+    "revision": "2858e4acbd3c7327d4a25cdc694e79c8",
+    "url": "/parcial-2/static/media/mascotas.2858e4ac.jpg"
+  },
+  {
+    "revision": "ff445ff064c4104e93793970fca1a714",
+    "url": "/parcial-2/static/media/matrix.ff445ff0.jpg"
+  },
+  {
+    "revision": "a82f5d905d28b27235159968a489e63e",
+    "url": "/parcial-2/static/media/mejor.a82f5d90.jpg"
+  },
+  {
+    "revision": "873c7a219b18d06e1d03a62ad1db8eda",
+    "url": "/parcial-2/static/media/minecraft.873c7a21.jpg"
+  },
+  {
+    "revision": "92a43bf41625798d50a0c8f0ea7b3336",
+    "url": "/parcial-2/static/media/mocca.92a43bf4.jpg"
+  },
+  {
+    "revision": "b51e40fb1ebb822acc8b636bda6b40d3",
+    "url": "/parcial-2/static/media/mowgli.b51e40fb.jpg"
+  },
+  {
+    "revision": "911e2a3980ba78150fed117a681c7081",
+    "url": "/parcial-2/static/media/nfs.911e2a39.jpg"
+  },
+  {
+    "revision": "d81690e67d1c4aa39c594dfa26ed66fb",
+    "url": "/parcial-2/static/media/nuqui.d81690e6.jpg"
+  },
+  {
+    "revision": "d3e773381b2a73a8a277ad573093e80f",
+    "url": "/parcial-2/static/media/pecador.d3e77338.jpg"
+  },
+  {
+    "revision": "ead691addfe1c2d02c8dd10ba2829663",
+    "url": "/parcial-2/static/media/productos.ead691ad.png"
+  },
+  {
+    "revision": "3f3188e23b21390d08dc02507176508f",
+    "url": "/parcial-2/static/media/purpura.3f3188e2.jpg"
+  },
+  {
+    "revision": "cb8182effa15bb861960e587226b5c66",
+    "url": "/parcial-2/static/media/putas.cb8182ef.jpg"
+  },
+  {
+    "revision": "57ec24b768d111d9d00cf1dbbf7dfb46",
+    "url": "/parcial-2/static/media/rimas.57ec24b7.jpg"
+  },
+  {
+    "revision": "6a5efacbaec1f82b3ae811b8b3355217",
+    "url": "/parcial-2/static/media/risen.6a5efacb.jpg"
+  },
+  {
+    "revision": "4adbce15743a0d6a8e3b76c8e15ead68",
+    "url": "/parcial-2/static/media/san.4adbce15.jpg"
+  },
+  {
+    "revision": "63e9b2ffe6a208357dc980bd47e3fab5",
+    "url": "/parcial-2/static/media/sentido.63e9b2ff.jpg"
+  },
+  {
+    "revision": "d4e9f2c4d8b439550153c739bf3833f1",
+    "url": "/parcial-2/static/media/sentimiento.d4e9f2c4.jpg"
+  },
+  {
+    "revision": "a348a0786cd27cf927f7823233592ae1",
+    "url": "/parcial-2/static/media/siddhartha.a348a078.jpg"
+  },
+  {
+    "revision": "79d1b7ee7966ce78b2fc28a03542f826",
+    "url": "/parcial-2/static/media/siempre.79d1b7ee.jpg"
+  },
+  {
+    "revision": "e16eaf43639c4f9e8c8aabd38ef1baea",
+    "url": "/parcial-2/static/media/sims.e16eaf43.jpg"
+  },
+  {
+    "revision": "f2552837c8a8cdf8da3760845056dcfd",
+    "url": "/parcial-2/static/media/tengo.f2552837.jpg"
+  },
+  {
+    "revision": "e552ee596e18fe8eaaea806a889636eb",
+    "url": "/parcial-2/static/media/tu-nombre.e552ee59.jpg"
+  },
+  {
+    "revision": "a9ac36ce5a019e3716630988430d72e6",
+    "url": "/parcial-2/static/media/unity.a9ac36ce.jpg"
+  },
+  {
+    "revision": "f16860430f2c0ce601ce98324d91771f",
+    "url": "/parcial-2/static/media/unreal.f1686043.jpg"
+  },
+  {
+    "revision": "578b8d5b3587c74c3b75a618d531de47",
+    "url": "/parcial-2/static/media/xampp.578b8d5b.jpg"
+  },
+  {
+    "revision": "35f1664a0c1497d0ee34e2b1cf7633b3",
+    "url": "/parcial-2/static/media/xd.35f1664a.jpg"
+  }
+]);
